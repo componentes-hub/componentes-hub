f1a09f4c3427666fb608bce8e96a3805d3368ba2
Javier Soria Blanco <javsorbla@alum.us.es>
2025-12-12 22:29:22 +0100

    fix: revert unnecesary session commit on functions

--- app/modules/auth/services.py
+++ app/modules/auth/services.py
@@ -124,7 +124,6 @@ class SessionDeviceService(BaseService):
 
         if session:
             self.repository.delete(session_id)
-            self.repository.session.commit()
             return True
         return False
 
@@ -139,6 +138,5 @@ class SessionDeviceService(BaseService):
         count = len(sessions_to_close)
         for session in sessions_to_close:
             self.repository.delete(session.id)
-        self.repository.session.commit()
 
         return count
